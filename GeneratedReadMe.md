1a14085c24546dea2531bda45284aebe019681b0
Pooja Agarwal <pooja@Poojas-MacBook-Air.local>
2022-10-21 23:28:13 -0700

    Adding code for Readme generation based on user input

---
+++ GeneratedReadMe.md
@@ -0,0 +1,37 @@
+
+## Title
+Readme Generator
+
+## Table of Contents
+- [Description](#description)
+- [Installation Instructions](#installation-instructions)
+- [Usage Information](#usage-information)
+- [Contribution Guidelines](#contribution-guidelines)
+- [Test Instructions](#test-instructions)
+- [License](#license)
+- [Github Username](#github-username)
+- [Email](#email)
+
+## Description
+A node.js application that takes user input to generate a Readme file.
+
+## Installation Instructions
+npm init
+
+## Usage Information
+node index.js
+
+## Contribution Guidelines
+Pooja Agarwal
+
+## Test Instructions
+Test that the generated readme file has the right sections .
+
+## License
+MIT
+
+## Github Username
+mypooja
+
+## Email
+khushi23.1987@gmail.com
--- index.js
+++ index.js
@@ -1,58 +1,103 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'title',
-      message: 'What is your title of project?',
-    },
-    {
+import inquirer from 'inquirer';
+import fs from 'fs';
+
+  inquirer
+    .prompt([
+      {
         type: 'input',
-        name: 'describtion',
-        message: 'write describtion of ur project?',
+        name: 'title',
+        message: 'What is the title of your project?',
       },
       {
         type: 'input',
-        name: 'contents',
-        message: 'write about table of contents?',
+        name: 'description',
+        message: 'What is the description of your project?',
       },
-    {
-      type: 'input',
-      name: 'inst',
-      message: 'write installation instructions?',
-    },
-    {
-      type: 'input',
-      name: 'usage',
-      message: 'usage information',
-    },
-    {
-      type: 'input',
-      name: 'guidelines',
-      message: 'What is contribution guidelines?',
-    },
-    {
-    type: 'input',
-      name: 'instructions',
-      message: 'What is test instructions?',
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username',
-    },
-    {
-      type: 'input',
-      name: 'linkedin',
-      message: 'Enter your LinkedIn URL.',
-    },
-  ])
-  .then((answers) => {
-    const readmePageContent = generatereadme(answers);
+      {
+        type: 'input',
+        name: 'instructions',
+        message: 'What are the installation instructions of your project?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What are the usage information of your project?',
+      },
+      {
+        type: 'input',
+        name: 'guidelines',
+        message: 'What are the contribution guidelines of your project?',
+      },
+      {
+        type: 'input',
+        name: 'testInstructions',
+        message: 'What are the test instructions of your project?',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Choose the license for this project?',
+        choices: [
+          "GNU",
+          "MIT",
+          "Open"
+        ]
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email.',
+      },
+    ])
+    .then((answers) => {
+      const readmePageContent = generateReadMe(answers);
 
-    fs.writeFile('README.md', readmePageContent, (err) =>
-      err ? console.log(err) : console.log('Successfully created README.md!')
-    );
-  });
+      fs.writeFile('GeneratedReadMe.md', readmePageContent, (err) =>
+        err ? console.log(err) : console.log('Successfully created README.md!')
+      );
+    });
 
+const generateReadMe = ({ title, description, instructions, usage, guidelines, testInstructions, license, github,  email }) =>
+`
+## Title
+${title}
+
+## Table of Contents
+- [Description](#description)
+- [Installation Instructions](#installation-instructions)
+- [Usage Information](#usage-information)
+- [Contribution Guidelines](#contribution-guidelines)
+- [Test Instructions](#test-instructions)
+- [License](#license)
+- [Github Username](#github-username)
+- [Email](#email)
+
+## Description
+${description}
+
+## Installation Instructions
+${instructions}
+
+## Usage Information
+${usage}
+
+## Contribution Guidelines
+${guidelines}
+
+## Test Instructions
+${testInstructions}
+
+## License
+${license}
+
+## Github Username
+${github}
+
+## Email
+${email}
+`;
